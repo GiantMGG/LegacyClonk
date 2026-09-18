49df3d16d2c4ae09e82400881ea84d3b6348e459
Max Grobbel <max.grobbel@gmail.com>
2026-09-18 18:23:55 +0200

    chore(version): bump content version to 4.9.11.7 for v370

--- src/C4Version.h
+++ src/C4Version.h
@@ -28,8 +28,8 @@
 #define C4XVER1 4
 #define C4XVER2 9
 #define C4XVER3 11
-#define C4XVER4 6
-#define C4XVERBUILD 369
+#define C4XVER4 7
+#define C4XVERBUILD 370
 #define C4VERSIONEXTRA ""
 /* These values are now controlled by the file source/version - DO NOT MODIFY DIRECTLY */
 
